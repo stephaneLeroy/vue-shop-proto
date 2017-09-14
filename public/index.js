42ec655e59363fa99ac916a0df2efdf75110d7b9
Stephane Leroy <ste.leroy@gmail.com>
2017-09-15 01:53:46 +0200

    Simple vue integration

--- public/index.js
+++ public/index.js
@@ -1,5 +1,43 @@
+var eventBus = new Vue()
 
+var products = Vue.component('products',{
+  template: '#products',
+  data: function () {
+    return { products: [{
+      "id": 1,
+      "name": "Rouge à lèvre",
+      "price": "",
+      "img": "/assets/rouge_a_levres.jpg"
+    }] }
+  }
+});
+
+var product = Vue.component('product',{
+  template: '#product',
+  props: ['product'],
+  methods: {
+    addToCart: function () {
+      eventBus.$emit('addToCart', this.product)
+    }  
+  }
+});
 
 new Vue({
-  el: '#main'
+  el: '#main',
+  data: function () {
+    return {
+      cart: []
+    }
+  },
+  computed: {
+    cartCount: function () {
+      return this.cart.length
+    }
+  },
+  mounted: function () {
+    var that = this
+    eventBus.$on('addToCart', function (product) {
+      that.cart.push(product)
+    })
+  }
 });
